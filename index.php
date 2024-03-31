dcc7c5f38b4358daa65318b7e24b2cd0bcfa8759
faishalnafi <faishalnafi50@gmail.com>
2024-03-31 12:14:47 +0700

    Program Master

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//Komentar 1 baris;
+/*
+Komentar juga;
+Namun multiline;
+*/
+
+// mengularkan ouput
+/*
+echo, print
+//untuk debuging
+print_r (untuk mencetak array);
+var_dump (melihat isi variabel);
+*/
+echo "Faishal Nafi";
+
+?>
