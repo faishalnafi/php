9642b676870692b35f6aa69423d0e8c141691606
faishalnafi <faishalnafi50@gmail.com>
2025-03-18 21:46:36 +0700

    controller: parse url

--- Libs/mvc/app/core/App.php
+++ Libs/mvc/app/core/App.php
@@ -1,11 +1,22 @@
 <?php
 
+use Soap\Url;
+
 class App {
+    protected $controller = 'Home';
+    protected $metod = 'index';
+    protected $params = [];
+
     public function __construct()
     {
         // echo "Ok, Afii!";
         $url = $this->parseURL();
-        var_dump($url);
+        // var_dump($url);
+
+        if (file_exists('../app/controllers/'.$url[0].'.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
     }
 
     public function parseURL() {
