c1b4740a7ac7754842e2c87d74e2447ed9085a71
faishalnafi <faishalnafi50@gmail.com>
2025-05-25 18:26:11 +0700

    jenis output php

---
+++ Libs/phpdasar/l2/output.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "standar  output php | ";
+// standar output
+
+/*
+echo, print
+print_r > untuk cetak isi array
+var_dump > melihat isi variabel
+*/
+
+echo "ini mengunakan ECHO | ";
+print "ini menggunakan PRINT | ";
+print_r ("ini menggunakan PRINT_R | ");
+var_dump ("ini menggunakan VAR_DUMP!");
