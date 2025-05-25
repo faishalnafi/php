4a71eb5b8de99545c948764db4e1aa8b159c1b40
faishalnafi <faishalnafi50@gmail.com>
2025-05-25 18:42:29 +0700

    sintaks php

--- Libs/phpdasar/l2/output.php
+++ Libs/phpdasar/l2/output.php
@@ -13,3 +13,9 @@ echo "ini mengunakan ECHO | ";
 print "ini menggunakan PRINT | ";
 print_r ("ini menggunakan PRINT_R | ");
 var_dump ("ini menggunakan VAR_DUMP!");
+
+echo " | untuk string menggunakan kutip 2 | ";
+echo 'menggunakan kutip 1 | ';
+echo 12345;
+echo true;
+echo false;
---
+++ Libs/phpdasar/l2/sintaks.php
@@ -0,0 +1,25 @@
+<?php
+// penulisan sintaks dalam php
+/*
+1. php dalam html
+2. html dalam php               ## ini tidak disarankan !!!
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar sintaks php</title>
+</head>
+<body>
+    <!-- penulisan sintaks php dalam html -->
+    <h1>halo, bagaimana kabarmu <?php echo "faishalnafi ?!!";?></h1>
+
+    <!-- penulisan sintaks html dalam php -->
+    <?php
+        echo "<h1>penulisan html dalam tag php</h1>";
+    ?>
+</body>
+</html>
