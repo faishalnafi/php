f621a7d5944cc194cdec3cdb9edb87e6153e2fca
faishalnafi <faishalnafi50@gmail.com>
2025-05-25 19:30:24 +0700

    var php

---
+++ Libs/phpdasar/l2/var.php
@@ -0,0 +1,10 @@
+<?php
+
+// variabel dalam php
+// variabel tidak boleh diawali dengan angka, tp boleh mengandung angka;
+
+$nama = "faishalnafi";     //dalam php sama seperti js tidak perlu mendefinisiakan tipe data;
+
+echo "Hello, $nama! <br>";
+// bila menggunakan kutip satu maka tidak akan di eksekusi;
+echo 'Hello, $nama';
