5e341ed5c9cfc6d149b0b23dcf397fd1c6780889
faishalnafi <faishalnafi50@gmail.com>
2025-05-26 18:29:33 +0700

    pengondisian php

--- Libs/phpdasar/l3/pengondisian.php
+++ Libs/phpdasar/l3/pengondisian.php
@@ -1,5 +1,5 @@
 <?php
-// pengondisian
+// pengondisian / percabangan
 /*
 if else
 if elseif else
@@ -8,3 +8,39 @@ switch
  */
 
 echo "pengondisian <br>";
+$x=10;
+$p=20;
+if($x < $p) {
+    echo "Benar <br><br>";
+}
+
+$x=50;
+$p=20;
+if($x < $p) {
+    echo "Benar <br>";
+}
+echo "Salah <br><br>";
+
+$x=10;
+$p=20;
+if($x < $p) {
+    echo "Benar <br>";
+}
+echo "Salah <br><br>";
+
+$x=50;
+$p=20;
+if($x < $p) {
+    echo "Benar <br>";
+} else {
+echo "Salah <br><br>";
+}
+
+$x=50;
+if($x < 10) {
+    echo "Benar <br>";
+} elseif ($x == 50) {           //bisa mengguakan else if atau disambung elseif
+    echo "Bingo!! <br><br>";
+} else {
+echo "Salah <br><br>";
+}
