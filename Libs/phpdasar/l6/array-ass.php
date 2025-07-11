9cca1b5a5a65a26d8689c793dc7fd0f90543c94b
faishalnafi <faishalnafi50@gmail.com>
2025-07-11 13:32:03 +0700

    array associative l6

---
+++ Libs/phpdasar/l6/array-ass.php
@@ -0,0 +1,21 @@
+<?php
+
+// array associative
+$mahasiswa = [
+    [
+        "name" => "faishalnafi'", 
+        "id" => "12345", 
+        "jurusan" => "SI"
+    ],
+    [
+        "name" => "ucupsurucup",
+        "id" => "67890",
+        "jurusan" => "SI"
+    ]
+];
+
+// print_r($mahasiswa);
+
+echo $mahasiswa[0]["name"];
+
+?>  
