8af0cc919e022cf18403dd974b40a2302f36d539
faishalnafi <faishalnafi50@gmail.com>
2025-08-07 06:23:24 +0700

    add gambar

--- Libs/phpdasar/l6/array-ass.php
+++ Libs/phpdasar/l6/array-ass.php
@@ -5,12 +5,14 @@ $mahasiswa = [
     [
         "name" => "faishalnafi'", 
         "id" => "12345", 
-        "jurusan" => "SI"
+        "jurusan" => "SI",
+        "gambar" => "avatar.jpg"
     ],
     [
         "name" => "ucupsurucup",
         "id" => "67890",
-        "jurusan" => "SI"
+        "jurusan" => "SI",
+        "gambar" => "avatar1.jpg"
     ]
 ];
 
@@ -18,4 +20,31 @@ $mahasiswa = [
 
 echo $mahasiswa[0]["name"];
 
-?>  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arra asosiatif</title>
+    <style>
+        .hero {
+            width: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1><b>DATA MAHASISWA</b></h1>
+    <?php foreach($mahasiswa as $mhs): ?>
+        <ul>
+            <li>
+                <img src="img/<?= $mhs["gambar"] ?>" alt="hero" class="hero">
+            </li>
+            <li><?= $mhs["name"]; ?></li>
+            <li><?= $mhs["id"]; ?></li>
+            <li><?= $mhs["jurusan"]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+</html>
