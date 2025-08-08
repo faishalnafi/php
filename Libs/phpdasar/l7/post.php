4d7ff816467084b893245f1863a84777cc398299
faishalnafi <faishalnafi50@gmail.com>
2025-08-08 14:26:57 +0700

    request post

---
+++ Libs/phpdasar/l7/post.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>request post metode</title>
+</head>
+<body>
+    <form action="" method="">
+        Masukkan Nama
+        <input type="text" name="nama">
+        <br>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
